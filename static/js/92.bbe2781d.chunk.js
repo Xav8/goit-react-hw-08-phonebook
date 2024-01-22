45307a5a387a390be3271e2ae6119bbc9820fba6
Xav8 <108630501+Xav8@users.noreply.github.com>
2024-01-22 12:09:07 +0000

    Deploying to gh-pages from @ Xav8/goit-react-hw-08-phonebook@a30aacb4c07bfb7dc4f41056c230721ce0559452 🚀

---
+++ static/js/92.bbe2781d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[92],{7092:(e,t,o)=>{o.r(t),o.d(t,{default:()=>c});var s=o(8935),r=o(6559),a=o(7689),i=o(184);const c=()=>{const e=(0,a.s0)();return(0,i.jsx)(s.ZP,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:(0,i.jsx)(r.ZP,{type:"primary",onClick:()=>e("/"),children:"Back Home"})})}}}]);
+//# sourceMappingURL=92.bbe2781d.chunk.js.map
